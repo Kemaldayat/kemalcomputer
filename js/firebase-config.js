9ae9825a96b6398637402ac8407276af6baa7a69
Kemaldayat <106178915+Kemaldayat@users.noreply.github.com>
2026-05-06 09:16:47 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
-import { getDatabase, ref as dbRef, onValue, set, remove, get, child } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
+import { getDatabase, ref as dbRef, onValue, set, remove, get, child, push } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
 import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js";
 
 const firebaseConfig = {
@@ -41,5 +41,6 @@ export {
     child, 
     storageRef, 
     uploadBytes, 
-    getDownloadURL 
+    getDownloadURL,
+    push 
 };
